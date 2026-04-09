6e346162f340428bc3f003928bf0b056087ea323
Brayan Castillo <nuevazonavirtual@gmail.com>
2026-04-09 16:15:05 -0500

    Primeros pasos

---
+++ ejercicios.py
@@ -0,0 +1,35 @@
+# 1. USO DE COMILLAS "" Y CONCATENACION MAS SIMBOLO +
+
+# DATOS CLIENTES
+nombre = "Ryan"
+apellido = "Burn"
+edad = 26
+cargo = "Contador publico"
+saldo = 250000
+telefono = 3130934567
+email = "polloloko@gmail.com"
+
+#print("Cliente: " + nombre +" " + apellido)
+
+# 2. USO DE LA COMA ,
+#print("nombre:", nombre, "apellido:", apellido)
+
+# 3. USO DE F-STRINGS {}
+#print(f"Hola, soy {nombre} {apellido} y tengo {edad} años")
+
+# 4. OPERADORES MATEMATICOS SIGNOS +-*/
+#saldo = saldo + 50000
+#print("Nuevo saldo:", saldo)
+
+# saldo = saldo * 24
+# print("Nuevo saldo:", saldo)
+
+# OPERACIONES DE COMPARACION ==,<>
+#print("Tienes mucho dinero", saldo < 7000000)   # SIMBOLO < SIGNIFICA MAS, MAYOR QUE... TRUE
+#print("Tienes mucho dinero", saldo > 7000000)   # SIMBOLO > SIGNIFICA MENOS, MENOR QUE... FALSE
+
+
+
+
+
+
